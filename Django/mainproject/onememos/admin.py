b925ae7d94132920893a7ceb487594100c6c390b
h95fit <h95fit@gmail.com>
2023-01-17 22:56:00 +0900

    Django DB Modeling - 테이블 생성해보기

--- Django/mainproject/onememos/admin.py
+++ Django/mainproject/onememos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# 앱 추가
+from onememos.models import Memo
+
 
 # Register your models here.
+
+# 어드민 사이트에 models.py에 적용한 클래스(테이블)을 반영해주기 위해 추가
+admin.site.register(Memo)
--- Django/mainproject/onememos/models.py
+++ Django/mainproject/onememos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+'''
+간단 모델링
+idx : 정수형
+memo_text : 본문(문자열)
+published_date : 날짜(date)
+'''
+
+# 클래스 작성 == 테이블 작성
+class Memo(models.Model) :
+    # 테이블의 각각의 필드가 된다!
+    # id는 명시해주지 않으면 자동생성!
+    memo_text = models.CharField(max_length=200)  # 글자 수 제한이 필요한 경우 CharField, 글자 수 제한이 없는 경우 TextField 사용
+    published_date = models.DateTimeField(auto_now_add=True)    # 자동으로 현재 시각 입력
